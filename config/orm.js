594d59799d845f2732aba16c2ea3cdf881c523a7
ctghadiri <ctghadiri@gmail.com>
2019-04-04 09:57:07 -0700

    add uneaten and devoured functions for controller

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,24 @@
 var connection = require('connection.js');
 
+var orm = {
+    uneaten: function(col, table, condition, cb){
+        var queryString = 'SELECT ' + col; 
+        queryString += ' FROM ' + table ;
+        queryString += ' WHERE ' + condition;
+
+        connection.connect(queryString,function (err, result){
+            if (err) throw err;
+            cb(result)
+        })
+    },
+    devoured: function(){
+        var queryString = 'SELECT ' + col; 
+        queryString += ' FROM ' + table ;
+        queryString += ' WHERE ' + condition;
+
+        connection.connect(queryString,function (err, result){
+            if (err) throw err;
+            cb(result)
+        })
+    }
+}
