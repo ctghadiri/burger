5e408b06998fb914e40733100ad1695aab1a330c
ctghadiri <ctghadiri@gmail.com>
2019-04-03 20:57:29 -0700

    require and set handlebars

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}))
 app.use(express.json());
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
