477fc47659b6b310d3f58ac8cce42544e5b14d57
ctghadiri <ctghadiri@gmail.com>
2019-04-03 20:30:17 -0700

    adjust table

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
