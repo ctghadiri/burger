61975036380e71f61e2921b0cdb7764f79f0759a
ctghadiri <ctghadiri@gmail.com>
2019-04-04 12:07:33 -0700

    add all function to orm

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,20 @@
 var connection = require('connection.js');
 
 var orm = {
+    all: function (table, cb){
+        var queryString = 'SELECT * FROM ';
+        queryString += table;
+        connection.query(queryString, function (err, result){
+            if(err)throw err;
+            cb(result)
+        })
+    },
     uneaten: function(col, table, condition, cb){
         var queryString = 'SELECT ' + col; 
         queryString += ' FROM ' + table ;
         queryString += ' WHERE ' + condition;
 
-        connection.connect(queryString,function (err, result){
+        connection.query(queryString,function (err, result){
             if (err) throw err;
             cb(result)
         });
@@ -16,7 +24,7 @@ var orm = {
         queryString += ' FROM ' + table ;
         queryString += ' WHERE ' + condition;
 
-        connection.connect(queryString,function (err, result){
+        connection.query(queryString,function (err, result){
             if (err) throw err;
             cb(result)
         });
