6f51cf7d79f1647b1c5adc161c317b4204cde43e
ctghadiri <ctghadiri@gmail.com>
2019-04-05 19:17:56 -0700

    change single quote to double and adjust variable name

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,26 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var burgers = require('../models/burger.js');
+var burger = require("../models/burger.js");
 
-router.get('/', function (req, res){
-    burgers.all(function (data){
+router.get("/", function (req, res){
+    burger.all(function (data){
         var hbsObject = {
             burgers: data
         };
-        res.render('index', hbsObject);
+        console.log(hbsObject)
+        res.render("index", hbsObject);
     });
 });
 
-router.post('/api/burger', function(req,res){
-    burgers.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function(result){
+router.post("/api/burger", function(req,res){
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result){
         res.json({id: result.insertId});
     });
 });
 
-router.put('/api/burger/:id', function (req, res){
-    var condition = 'id = ' + req.params.id;
-    burgers.update(
+router.put("/api/burger/:id", function (req, res){
+    var condition = "id = " + req.params.id;
+    burger.update(
         {
             devoured: req.body.devoured
         },
