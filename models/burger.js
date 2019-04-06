6c7abdeda42d730e4f5386d7c27bd4cc68125a43
ctghadiri <ctghadiri@gmail.com>
2019-04-05 19:22:49 -0700

    change quotation to double quotes

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,18 @@
-var orm = require('../config/orm.js');
+var orm = require("../config/orm.js");
 
 var burger = {
     all: function(cb){
-        orm.all('burgers', function (res){
+        orm.all("burgers", function (res){
             cb(res);
         });
     },
     update: function(colsVals, condition, cb){
-        orm.update('burgers',  colsVals, condition, function(res){
+        orm.update("burgers",  colsVals, condition, function(res){
             cb(res)
         });
     },
     create: function(cols, vals, cb){
-        orm.create('burgers', cols, vals, function (res){
+        orm.create("burgers", cols, vals, function (res){
             cb(res);
         });
     }
